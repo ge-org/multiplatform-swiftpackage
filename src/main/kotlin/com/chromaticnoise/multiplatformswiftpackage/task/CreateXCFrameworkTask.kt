c447734661495c15212ade8157c284e0eeba08c5
Val <grachev@sibext.com>
2022-06-03 13:53:42 +0700

    Set name of Universal framework same as baseName configurated binaries

--- src/main/kotlin/com/chromaticnoise/multiplatformswiftpackage/task/CreateXCFrameworkTask.kt
+++ src/main/kotlin/com/chromaticnoise/multiplatformswiftpackage/task/CreateXCFrameworkTask.kt
@@ -110,7 +110,7 @@ internal fun Project.registerCreateUniversalIosSimulatorFrameworkTask() =
         dependsOn(targets.map { it.linkTask.name })
         if (targets.isNotEmpty()) {
             val buildType = if (targets[0].linkTask.name.contains("Release")) "release" else "debug"
-            baseName = configuration.packageName.value
+            baseName = checkNotNull(targets.first().name.value)
             destinationDir = buildDir.resolve("bin/iosSimulatorUniversal/${buildType}Framework")
             from(targets.mapNotNull { it.framework })
         }
@@ -126,7 +126,7 @@ internal fun Project.registerCreateUniversalWatchosSimulatorFrameworkTask() =
         dependsOn(targets.map { it.linkTask.name })
         if (targets.isNotEmpty()) {
             val buildType = if (targets[0].linkTask.name.contains("Release")) "release" else "debug"
-            baseName = configuration.packageName.value
+            baseName = checkNotNull(targets.first().name.value)
             destinationDir = buildDir.resolve("bin/watchosSimulatorUniversal/$buildType")
             from(targets.mapNotNull { it.framework })
         }
@@ -142,7 +142,7 @@ internal fun Project.registerCreateUniversalTvosSimulatorFrameworkTask() =
         dependsOn(targets.map { it.linkTask.name })
         if (targets.isNotEmpty()) {
             val buildType = if (targets[0].linkTask.name.contains("Release")) "release" else "debug"
-            baseName = configuration.packageName.value
+            baseName = checkNotNull(targets.first().name.value)
             destinationDir = buildDir.resolve("bin/tvosSimulatorUniversal/$buildType")
             from(targets.mapNotNull { it.framework })
         }
